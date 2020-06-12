f09b1bea6931cfca1ec33a6c43f9a112a0e7fd94
hakan ölçer <ha_-kan-_olcer95@windowslive.com>
2020-06-12 20:23:21 +0300

    ShopContext

--- src/Web/Startup.cs
+++ src/Web/Startup.cs
@@ -13,6 +13,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Infrastructure.Identity;
+using Infrastructure.Data;
 
 namespace Web
 {
@@ -31,6 +32,10 @@ namespace Web
             services.AddDbContext<AppIdentityDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("IdentityConnection")));
+
+            services.AddDbContext<ShopContext>(options =>
+               options.UseSqlServer(
+                   Configuration.GetConnectionString("ShopConnection")));
 
             services.AddIdentity<ApplicationUser, IdentityRole>()
                 .AddDefaultUI().AddEntityFrameworkStores<AppIdentityDbContext>()
